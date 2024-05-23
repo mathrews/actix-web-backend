a245532d84c1652ffe2a0e18f02109d7bc87fef3
mathrews <mateusps4gg@gmail.com>
2024-05-23 16:31:04 -0300

    first commit

---
+++ ActixWebTaskService/src/main.rs
@@ -0,0 +1,23 @@
+mod api;
+use api::task::{
+    get_task
+};
+
+use actix_web::{HttpServer, App, web::Data, middleware::Logger};
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    std::env::set_var(key: "RUST_LOG", value: "debug");     
+    std::env::set_var(key: "RUST_BACKTRACE", value: "1");     
+    env_logger::init();
+
+    HttpServer::new(move || {
+        let logger = Logger::default();
+        App::new()
+        .wrap(logger)
+        .service(get_task)
+    })
+    .bind(("127.0.0.1", 80))?
+    .run()
+    .await
+}
